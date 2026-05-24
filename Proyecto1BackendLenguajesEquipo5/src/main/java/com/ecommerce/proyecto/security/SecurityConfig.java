db3f50ca0d9965b0d52408c969c91fbe47bd20bf
cbastiancq-lab <cbastiancq@gmail.com>
2026-05-24 12:40:39 -0600

    GET LISTO

--- Proyecto1BackendLenguajesEquipo5/src/main/java/com/ecommerce/proyecto/security/SecurityConfig.java
+++ Proyecto1BackendLenguajesEquipo5/src/main/java/com/ecommerce/proyecto/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
@@ -40,7 +41,7 @@ public class SecurityConfig {
 			)
 			.formLogin(formLogin -> formLogin.disable())
 			.httpBasic(basic -> basic.disable())
-			//.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			.exceptionHandling(exception -> exception
 				.authenticationEntryPoint((request, response, authException) -> {
 					response.setStatus(401);
